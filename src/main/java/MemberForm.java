a17a5b47787d3dc3900786648283d57fc6ecee31
austin wamono <austinwamono98@gmail.com>
2025-11-07 14:38:39 +0300

    Commit#2: creating instances of the different JComponents in MemberForm class and constructor to hold the JFrame

--- src/main/java/MemberForm.java
+++ src/main/java/MemberForm.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.table.DefaultTableModel;
 public class MemberForm {
+    JTextField nametextfield;
+    JTextField emailField;
+    JButton saveButton;
+    JTable MemberTable;
+    DefaultTableModel tableModel;
+    JFrame Mainframe;
+    JPanel Formpanel;
+    JLabel namelabel,emaillabel;
+    //constructor
+    public MemberForm(){
+        this.prepareJFrame();
+    }
+
 }
