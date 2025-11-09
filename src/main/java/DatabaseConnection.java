2a47b9f9846215df1b9433a7338a55306d13f761
awabqais <awab.qais@gmail.com>
2025-11-09 15:28:40 +0300

    commit#10
    in the NV we creat method to connect the project with the database

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -2,4 +2,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DatabaseConnection {
+    //details of sql server
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=TestDB;encrypt=true;trustServerCertificate=true;";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "put your sql password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
